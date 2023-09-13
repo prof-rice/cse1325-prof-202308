bbfbc1237dd701926291c703251876b8e563fbce
Professor Rice <george.rice@uta.edu>
2023-09-13 18:51:00 -0500

    P03 add license notice

--- P03/extreme_bonus/Publication.java
+++ P03/extreme_bonus/Publication.java
@@ -1,3 +1,9 @@
+// Copyright 2023 George F. Rice <https://github.com/prof-rice>
+//
+// This file is part of the Library Management System and is licensed
+// under the terms of the Gnu General Public License version 3 or 
+// (at your option) any later version, see <https://www.gnu.org/licenses/>.
+
 import java.time.LocalDate;
 
 class Publication {
