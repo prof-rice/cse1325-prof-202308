f944955d62f4f8964c5f1852883913ee2e2bf87d
Professor Rice <george.rice@uta.edu>
2023-09-19 17:56:58 -0500

    Practice Exams #1 code

---
+++ E1/practice3/custom_exception/UtaException.java
@@ -0,0 +1,7 @@
+class UtaException extends Exception {
+    public UtaException() {super();}
+    public UtaException(String message) {super(message);}
+    public UtaException(Throwable t) {super(t);}
+    public UtaException(String message, Throwable t) {super(message, t);}
+}
+    
